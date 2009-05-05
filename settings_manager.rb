aeeead7ecba147a533e791dceb4bf9e0c505ec2f
Riccardo Iaconelli <riccardo@kde.org>
2009-05-05 09:51:42 +0200

    add methods to check if Repo and Location are complete, completely port to hashes
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- settings_manager.rb
+++ settings_manager.rb
@@ -1,42 +1,84 @@
 
 class Location
-  attr_accessor :name, :url
+  attr_accessor :name, :path
+  
+  def complete?
+    begin
+      if !name.empty? and !path.empty?
+        return true
+      end
+    rescue NoMethodError
+      return false
+    end
+  end
 end
 
 class Repo
   attr_accessor :location, :type,
                 :name, :url
+  
+  def complete?
+    begin
+      if !name.empty? && !url.empty? &&
+         location.complete? && !type.empty?
+        return true
+      end
+    rescue NoMethodError
+      return false
+    end
+  end
 end
 
 class SettingsManager
 
   def initialize
     @repos = Hash.new
-    
+    @locations = Hash.new
     reparse_config
   end
 
   def reparse_config
-    @repos[:home] = Array.new
+    i = 0
     IO.foreach($config_file) do |line|
-      line.lstrip!
-      # skip comments
+      # counter
+      i = i + 1
+      
+      # don't parse carriage returns and extra spaces!
+      line.strip!
+      
+      # skip comments, empty line
       next if line.start_with? '#'
+      next if line.empty?
+      
       # first, locations!
       if line.start_with? '['
         c = line.split('=')
         c[0].gsub!('[', '').gsub!(']', '')
-        puts c
+        l = Location.new
+        l.name = c[0]
+        l.path = c[1]
+        @locations[l.name.to_sym] = l
+        @repos[l.name.to_sym] = Array.new
+        if !l.complete?
+          puts "Malformed configuration line -- #{$config_file}:#{i}"
+          puts ">> #{line}"
+          exit 1
+        end
         next
       end
+      
+      # if we arrive here, it's a repo config.
       config = line.split(',')
-      if config.size == 4
-        repo = Repo.new
-        repo.location = config[0]
-        repo.type = config[1]
-        repo.name = config[2]
-        repo.url = config[3]
-        @repos[repo.location.to_sym] << repo
+      repo = Repo.new
+      repo.location = @locations[config[0].to_sym]
+      repo.type = config[1]
+      repo.name = config[2]
+      repo.url = config[3]
+      @repos[repo.location.name.to_sym] << repo
+      if !repo.complete?
+        puts "Malformed configuration line -- #{$config_file}:#{i}"
+        puts ">> #{line}"
+        exit 1
       end
     end
   end
