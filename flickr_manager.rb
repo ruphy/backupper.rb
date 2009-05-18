47577cbac2345b153b90448754e6ccfa2535fb04
Riccardo Iaconelli <riccardo@kde.org>
2009-05-18 09:14:57 +0200

    add just a skeleton for a flickr manager =)
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- flickr_manager.rb
+++ flickr_manager.rb
@@ -1,3 +1,20 @@
 
+$token = "#{File.expand_path('~')}/.ruphy-backupper/flickr-token.cfg"
 
+class FlickrManager
+  
+  def initialize
+    
+  end
+  
+  def login user
+    if (token_valid)
+      
+    else
+      
+    end
+  end
+  
+  
+end
 
