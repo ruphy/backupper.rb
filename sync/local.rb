f03ed6cd0a653bca8318a5d299ffc9026ba4eb95
Riccardo Iaconelli <riccardo@kde.org>
2009-05-23 13:27:09 +0200

    add some relevant files from ruby-sync. add a flickr class
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

---
+++ sync/local.rb
@@ -0,0 +1,27 @@
+# This class is from the ruby-sync gem
+
+class Local < Sources::Base
+  def initialize()
+    
+  end
+  def file_list
+    files = []
+    dirs = ['.']
+    dirs += dir_list
+    dirs.each do |d|
+      files += Dir.entries(d).map { |f| d=='.' ? f : "#{d}/#{f}"}.select { |f| not File.directory?(f) }
+    end
+    files
+  end
+  def dir_list
+    in_dirs = ['.']
+    out_dirs = []
+    while (not in_dirs.empty?) do
+      dir = in_dirs.pop
+      new_dirs = Dir.entries(dir).select { |f| f != '.' && f != '..' }.map{ |f| dir=='.' ? f : "#{dir}/#{f}" }.select { |f| File.directory?(f) }
+      out_dirs += new_dirs
+      in_dirs += new_dirs
+    end
+    out_dirs
+  end
+end
