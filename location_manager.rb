1a6ea28fb1fd15127c6dacf035f2121896fda1e2
Riccardo Iaconelli <riccardo@kde.org>
2009-07-13 05:21:59 +0200

    move generic functions to location_manager
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- location_manager.rb
+++ location_manager.rb
@@ -1,7 +1,7 @@
 
 
 class LocationManager
-  attr_accessor :commit_makes_sense
+  attr_accessor :commit_makes_sense # bool, true if the commit (git meaning) concept makes sense in the backend
   
   def initialize location
     @location = location
@@ -20,6 +20,15 @@ class LocationManager
   
   def last_commit_date
     return nil if !@commit_makes_sense
+    
+  end
+  
+  def cd_path
+#     if @location.name == "gibak"
+#       cd File.expand_path '~'
+#     else
+      cd @location.path
+#     end
   end
   
 end
@@ -30,6 +39,44 @@ class GitLocationManager < LocationManager
     @commit_makes_sense = true
   end
 
+  def status
+    cd_path
+    return f_run("git status")
+  end
+  
+  def commit log
+    cd_path
+    if @location.name == "gibak"
+      run("gibak commit")
+    else
+      run("git commit -m \"#{log}\"")
+    end
+  end
+  
+  def working_dir_clean?
+    cd_path
+    @output = status
+    @output.each do |line|
+      next if line.start_with? '#'
+      return true if line.include? "working directory clean"
+    end
+    return false
+  end
+    
+  def current_branch
+    cd_path
+    f_run("git branch").each do |line|
+      return line.gsub!('* ', '') if line.include? '*'
+    end
+    return :no_branch
+  end
+  
+  def add
+    unless @location.name == "gibak"
+      cd @location.path
+      run("git add .")
+    end
+  end
   
 end
 
