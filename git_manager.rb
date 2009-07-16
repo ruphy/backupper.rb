b5c30db5e064798d71b5be0db97b4131b96163be
Riccardo Iaconelli <riccardo@kde.org>
2009-07-16 14:15:14 +0200

    another great simplification in gitmanager
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- git_manager.rb
+++ git_manager.rb
@@ -16,32 +16,17 @@ class GitManager
     @repo = repo
     @location = @repo.location unless @repo == nil
     @location = location if @repo == nil
-    @remotes = Array.new
-    add_remote repo.url
-  end
-
-  # Adds a remote
-  # remote: remote name
-  # branch: branch name, or :current_branch, which means the same as the branch
-  # that is currently checked out at runtime.
-  def add_remote remote, branch = :current_branch
-    branch = @repo.location.manager_for(:git).current_branch if branch == :current_branch
+    @remote = nil
+    
+    ## TODO: make it possible to have other branches too
+    branch = @repo.location.manager_for(:git).current_branch # if branch == :current_branch
     ErrorManager.warning "No branch checked out for location #{@repo.location.name}. Is it a git repo?" if branch == :no_branch
-    @remotes << Remote.new(remote, branch)
+    @remote = Remote.new(@repo.url, branch)
   end
 
-  def remotes
-    return @remotes
-  end
-
-  def push remote = :all
-    if remote == :all
-      @remotes.each do |r|
-        push r
-      end
-    else
-      run("git push #{remote.remote} #{remote.branch}")
-    end
+  def push
+    debug "pushing '#{@repo.location.name}' to '#{@repo.name}'"
+    run "git push #{@remote.remote} #{@remote.branch}"
   end
 
 end
